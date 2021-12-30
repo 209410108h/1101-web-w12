2da9452cc260ba870be89a6b29b08d43d4746ce3
209410108h <hank5947887@gmail.com>
2021-12-30 21:53:17 +0800

    1230 commit

--- src/App_08.js
+++ src/App_08.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import T33_08 from '../src/tutorial/4-forms/T43_08_crown2_create';
+//import T33_08 from '../src/tutorial/4-forms/T43_08_crown2_create';
+import Grocery_08 from './demo/grocery/Grocery_08';
 const App_08 = () => {
   return (
-    <div className='container'>
+    <Grocery_08 />
+    /*<div className='container'>
       <h2>Advanced Tutorial</h2>
       <T33_08 />
-    </div>
+    </div>*/
   );
 };
 
